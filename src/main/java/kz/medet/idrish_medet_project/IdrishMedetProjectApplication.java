addba2f31eb5baed1d1d7a92095683a87ca3f4d6
Medet Idrish <idrismedet0@gmail.com>
2024-12-18 22:21:53 +0500

    initial commit

---
+++ src/main/java/kz/medet/idrish_medet_project/IdrishMedetProjectApplication.java
@@ -0,0 +1,13 @@
+package kz.medet.idrish_medet_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdrishMedetProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IdrishMedetProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kz/medet/idrish_medet_project/IdrishMedetProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.medet.idrish_medet_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdrishMedetProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
